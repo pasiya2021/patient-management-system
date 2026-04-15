73bf12979108494b0cabb6b91882696ad8b47c1f
Pasindu Malinga <130921891+pasiya2021@users.noreply.github.com>
2026-04-15 09:15:59 +0530

    Fix: convert billing service from submodule to normal folder

---
+++ billinga-service/src/main/java/com/pm/billingaservice/BillingaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.billingaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingaServiceApplication.class, args);
+    }
+
+}
